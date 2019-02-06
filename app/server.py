ffdebf82633c71515b897cc57cb0939cf4f827da
Nick <nickkoenig11@gmail.com>
2019-02-06 15:36:32 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1qLHZJ-XWdu-KAU6WtnxBGjAJ20kMNca'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1qLHZJ-XWdu-KAU6WtnxBGjAJ20kMNcab'
 export_file_name = 'hockey.pkl'
 
 classes = ['ice_hockey', 'lacrosse', 'roller_hockey']
